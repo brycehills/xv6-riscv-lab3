74814ea91caa9bc46a80033c8e1c2b97374fc06d
Bryce H <hillsbryce@yahoo.com>
2022-05-27 15:17:44 -0700

    updated user.h

--- user/user.h
+++ user/user.h
@@ -40,3 +40,12 @@ void free(void*);
 int atoi(const char*);
 int memcmp(const void *, const void *, uint);
 void *memcpy(void *, const void *, uint);
+
+//lab 3 - added declarations
+struct lock_t {
+uint locked;
+};
+int thread_create(void *(start_routine)(void*), void *arg);
+void lock_init(struct lock_t* lock);
+void lock_acquire(struct lock_t* lock);
+void lock_release(struct lock_t* lock);
